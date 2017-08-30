b1483897a788b92a6908ab1ae0ab9b263349fda4
Eduardo Castro <educcjr@gmail.com>
2017-08-30 16:59:30 +0000

    fix unecessary keyfile.json require

--- api/index.js
+++ api/index.js
@@ -2,16 +2,15 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 
-const gcpAuth = {
+const Datastore = require('@google-cloud/datastore');
+const Storage = require('@google-cloud/storage');
+
+const gcpAuth = process.env.NODE_ENV === 'production' ? {} : {
   projectId: 'untitled-bot-174418',
   credentials: require('./keyfile.json')
 };
-const Datastore = require('@google-cloud/datastore');
-const datastore = process.env.NODE_ENV === 'production'
-  ? Datastore() : Datastore(gcpAuth);
-const Storage = require('@google-cloud/storage');
-const storage = process.env.NODE_ENV === 'production'
-  ? Storage() : Storage(gcpAuth);
+const datastore = Datastore(gcpAuth);
+const storage = Storage(gcpAuth);
 
 const RequestService = require('./../common/request-service.js');
 const requestService = new RequestService();
