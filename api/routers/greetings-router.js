a800551edbfab11c1cf09b0998e987eddcbaf8ad
Luiz Fernando Silva <luizinho_mack@yahoo.com.br>
2017-09-19 23:45:03 +0000

    Adicionando rotas para buscar áudios de usuários

--- api/routers/greetings-router.js
+++ api/routers/greetings-router.js
@@ -1,15 +1,40 @@
+const _ = require('lodash');
 const path = require('path');
 const express = require('express');
 const router = express.Router();
 
 const uploadDest = './../uploads/';
 const multer = require('multer');
-const uploadParser = multer({ storage: multer.diskStorage({
-  destination: uploadDest,
-  filename: (req, file, cb) => {
-    cb(null, Date.now() + path.extname(file.originalname));
+const uploadParser = multer({
+  storage: multer.diskStorage({
+    destination: uploadDest,
+    filename: (req, file, cb) => {
+      cb(null, Date.now() + path.extname(file.originalname));
+    }
+  })
+});
+
+router.get('/audio', (req, res, next) => {
+  const query = req.datastore.createQuery('AudioGreeting');
+
+  req.datastore
+    .runQuery(query)
+    .then((results) => { res.send(results[0]); });
+});
+
+router.get('/audio/:userId', (req, res, next) => {
+  if (!_.isInteger(Number.parseInt(req.params.userId))) {
+    return res.status(400).send({ message: 'usuário inválido' });
   }
-}) });
+
+  const query = req.datastore
+    .createQuery('AudioGreeting')
+    .filter('discordId', req.params.userId);
+
+  req.datastore
+    .runQuery(query)
+    .then((results) => { res.send(results[0]); });
+});
 
 router.post('/audio', uploadParser.single('file'), (req, res, next) => {
   if (path.extname(req.file.filename) !== '.mp3') {
@@ -32,7 +57,7 @@ router.post('/audio', uploadParser.single('file'), (req, res, next) => {
         if (err) return res.status(500).send(err);
       });
 
-      res.send({filename: req.file.filename});
+      res.send({ filename: req.file.filename });
     });
 });
 
