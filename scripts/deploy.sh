0bcff4866d30494f9b297893fece73c61bafeb88
Eduardo Castro <educcjr@gmail.com>
2017-09-30 21:36:41 +0000

    circle ci tests

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
-# TODO: remove ~/untitled-bot and clone again
+npm install pm2 -g
+pm2 stop all
+
+rm -r -f ~/untitled-bot
+git clone git@github.com:educcjr/untitled-bot.git
+cp  ~/scripts/app-configs.js ~/untitled-bot/bot/
+
 cd ~/untitled-bot
-git pull
-npm install
 NODE_ENV=production
+npm install
 npm start
